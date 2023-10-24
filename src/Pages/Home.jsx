7fbb6307ea9760c141fc45fdd5d0e3edf3a2bf27
NatSama2 <vet.nat.c@gmail.com>
2023-10-24 20:19:45 -0300

    barra superios y navbar casi listos, falta arreglar detalles del responsive y cambiar imagen

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,27 @@
 import Layout from "../Components/Layout/Index";
+import { Barrasuperior } from "../Components/Navbar/Index";
 
 export function Home() {
   return (
-    <Layout>
-    <p>Inicio</p>
-    </Layout>
-    );
-
+    <>
+      <Barrasuperior />
+      <Layout>
+{/*         <div className="inicio--descripciónbreve">
+          <p className="inicio-text-1">Certificados y const ancias</p>
+          <p className="inicio-text-2">
+            Para que las const ancias y certificados puedan emitirse, es
+            necesario que pasen por las 3 etapas de revisión: Logística
+            Contabilidad Fiscal En el menú lateral, podrás consultar las const
+            ancias y certificados pendientes de revisar, por firmar y las
+            aceptadas.
+          </p>
+          <p className="inicio-text-3">Puedes leer más dando click aquí</p>
+          <p className="inicio-text-4">
+            En este video te contamos más de la emisión de const ancias y
+            certificados
+          </p>
+        </div> */}
+      </Layout>
+    </>
+  );
 }
