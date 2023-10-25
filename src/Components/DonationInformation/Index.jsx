7ead03b31fd51dc66e228d0614fe63ee954930ad
carolinamoralese <caromorales1396@gmail.com>
2023-10-24 22:55:26 -0500

    se cambia la tabla para mostrar los documentos y se agrega paginacion

--- src/Components/DonationInformation/Index.jsx
+++ src/Components/DonationInformation/Index.jsx
@@ -1,70 +1,51 @@
 import React from "react";
+import { MDBDataTableV5 } from "mdbreact";
 
 export function DonationInformation({ documentos }) {
-  const propiedadFechaDeExpedicion = "Fecha Expedición";
-  const propiedadConsecutivo = "Consecutivo";
-  const propiedadEmpresa = "EMPRESA ";
-  const propiedadTipoDeCertificado = "Tipo_Certificado";
-  const conceptosDocumentos = {
-    "Donación_UnaAUno": "Donación uno a uno",
-    "Donación_Dinero": "Donación en dinero",
-    "Donación_Especie": "Donación en especie"
-  }
+  documentos.map((documento) => {
+    documento.accion = <a href="#">Ver - Editar</a>;
+    return documento;
+  });
+
+  const data = {
+    columns: [
+      {
+        label: "Fecha",
+        field: "Fecha Expedición",
+        sort: "asc",
+        width: 200,
+      },
+      {
+        label: "No. de constancia",
+        field: "Consecutivo",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Empresa",
+        field: "EMPRESA ",
+        sort: "asc",
+        width: 300,
+      },
+      {
+        label: "Concepto",
+        field: "Tipo_Certificado",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Acción",
+        field: "accion",
+        sort: "asc",
+        width: 100,
+      },
+    ],
+    rows: documentos,
+  };
 
   return (
-    <div className="flex space-x-4 mt-8 bg-gray-200 bg-opacity-50 font-bold p-4">
-     
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg--gris-oscuro-50 dark:bg-gris-oscuro-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-              Fecha
-              </th>
-              <th scope="col" className="px-6 py-3">
-                No. de constancia
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Empresa
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Concepto
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Acción
-              </th>
-            </tr>
-          </thead>
-
-          {documentos.map((documento, index) => (
-          <tbody key={index}>
-            <tr className="bg-white border-b dark:bg-gris-oscuro-900 dark:border-gray-700">
-              <td
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {documento[propiedadFechaDeExpedicion]}
-              </td>
-              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{documento[propiedadConsecutivo]}</td>
-              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{documento[propiedadEmpresa]}</td>
-              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{conceptosDocumentos[documento[propiedadTipoDeCertificado]]}</td>
-              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Ver- Editar
-                </a>
-              </td>
-            </tr>
-          </tbody>
-          ))}
-        </table>
-
-      </div>
-
-
-      
+    <div className="mt-8">
+      <MDBDataTableV5 striped bordered small data={data} />;
     </div>
   );
 }
